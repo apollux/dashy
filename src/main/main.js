f64572b10e999f38d1777ac91ac9f78f62a4671e
André Kwakernaak <mail@andrekwakernaak.xyz>
2019-05-14 20:43:40 +0200

    fixup! refactor: fix typo

--- src/main/main.js
+++ src/main/main.js
@@ -21,7 +21,7 @@ function createWindow(urls) {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    // carroussel = null;
+    // carrousel = null;
   });
 }
 
